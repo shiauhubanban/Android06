560233ef35d37d59a24d3210cb0b06ed853ac206
user <shiuhubanban>
2016-11-17 14:08:16 +0800

    v3

--- app/src/main/java/com/org/iii/shine06/MainActivity.java
+++ app/src/main/java/com/org/iii/shine06/MainActivity.java
@@ -4,14 +4,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv = (TextView)findViewById(R.id.tv);
     }
+
     public void test1(View v){
         MyThread mt1 = new MyThread();
         mt1.start();
@@ -21,8 +26,9 @@ public class MainActivity extends AppCompatActivity {
         public void run() {
             for (int i=0; i<10; i++){
                 Log.v("shine", "i = " + i);
+                //tv.setText("i = " + i);
                 try {
-                    Thread.sleep(200);
+                    Thread.sleep(500);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
