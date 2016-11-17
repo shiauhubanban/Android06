d151030076916e03d5e00846dc295b268ab3b7b3
user <shiuhubanban>
2016-11-17 14:05:42 +0800

    v2

--- app/src/main/java/com/org/iii/shine06/MainActivity.java
+++ app/src/main/java/com/org/iii/shine06/MainActivity.java
@@ -2,6 +2,8 @@ package com.org.iii.shine06;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void test1(View v){
+        MyThread mt1 = new MyThread();
+        mt1.start();
+    }
+    private class MyThread extends Thread {
+        @Override
+        public void run() {
+            for (int i=0; i<10; i++){
+                Log.v("shine", "i = " + i);
+                try {
+                    Thread.sleep(200);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
 }
